06b229bfb9e0c52a60809b01ca72215bc45c02c7
Joseph White <jpwhite4@buffalo.edu>
2017-08-03 16:15:43 -0400

    weq

--- update.sh
+++ update.sh
@@ -2,6 +2,8 @@
 
 versions=$(git branch -r | awk 'BEGIN{FS="/"} /^  origin\/xdmod/{print $2}')
 
+versions=$(curl -s https://api.github.com/repos/ubccr/xdmod-supremm/releases | jq .[].target_commitish | grep -o 'xdmod[0-9]\.[0-9]')
+
 for version in $versions;
 do
     filelist=$(git ls-tree --name-only -r origin/$version docs | egrep '*.md$')
